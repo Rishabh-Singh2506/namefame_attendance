3b844d73a35eb4cf5d76c162644c7ef4c080386d
Rishabh-singh2560.github.io <mr.rishabh.singh256@gmail.com>
2026-03-29 01:52:27 +0530

    Update App.js LINK TO SUPABASE

--- App.js
+++ App.js
@@ -6,8 +6,8 @@
 
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "https://YOUR_PROJECT_ID.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_ANON_KEY";
+const SUPABASE_URL = "https://qhikqbrfojdlmdwsdota.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoaWtxYnJmb2pkbG1kd3Nkb3RhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3MjcxMDgsImV4cCI6MjA5MDMwMzEwOH0.lYiBLoXPdNO_kfilcbX-OfbvJcXsjM841HG2ffwQT3Y";
 
 const supabase = createClient(
   SUPABASE_URL,
